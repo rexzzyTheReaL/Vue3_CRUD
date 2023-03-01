7ed340bc7d181ac0bd5576d7726ba590f699d892
razzy <82553581+rexzzyTheReaL@users.noreply.github.com>
2023-03-01 09:52:18 +0800

    Add files via upload
    bootstrap

--- Vue3_CRUD/EmployeeEdit.js
+++ Vue3_CRUD/EmployeeEdit.js
@@ -2,28 +2,40 @@ export default{
    props:{
    arr:Array
    },
-   emits:['doSave'],
+   emits:['doSave','doCloseEdit'],
    setup(props,{emit}) {
     const saveData = () => emit('doSave')
+    const closeEdit = () => emit('doCloseEdit')
     return{
         arr:props.arr,
-        saveData
+        saveData,
+        closeEdit
     }
    },
-   template:`<span>資料修改&nbsp;<input style="width: 70px;margin-bottom: 10px" type="button" @click="saveData" value="儲存"></span>
-<table>             
-<tr>
-    <th>員編</th>
-    <td><input v-model="arr[0].empNo" disabled></td>
-</tr>
-<tr>
-    <th>姓名</th>
-    <td><input v-model="arr[0].name"></td>
-</tr>
-<tr>
-    <th>分機</th>
-    <td><input v-model="arr[0].tel"></td>
-</tr>                      
-</table>
-   `
+   template:`
+   <span>資料修改</span>
+   <div class="border pt-2" style="padding:10px;">
+                <div class="row mb-2">
+                    <label class="col-sm-2 col-form-label">電腦編號</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" v-model="arr[0].empNo" disabled>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <label class="col-sm-2 col-form-label">姓名</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" v-model="arr[0].name">
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <label class="col-sm-2 col-form-label">分機</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" v-model="arr[0].tel">
+                    </div>
+                </div>
+                <div class="mb-2 text-end">                     
+                    <button type="button" class="btn btn-success" @click="saveData">儲存</button>&nbsp;
+                    <button type="button" class="btn btn-danger" @click="closeEdit">取消</button>
+                </div>
+    </div>`
 }
